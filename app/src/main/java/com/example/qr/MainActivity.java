114cbee856169f16ea5dadf502fe61a58be75b17
Vilkova Viktoria <vilkova_viktoria2000@mail.ru>
2021-04-24 20:16:20 +0500

    Полученный код отображается корректно на экране

--- app/src/main/java/com/example/qr/MainActivity.java
+++ app/src/main/java/com/example/qr/MainActivity.java
@@ -17,7 +17,7 @@ import androidx.core.content.ContextCompat;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     public Button scanBtn;
     public TextView formatTxt, contentTxt;
@@ -33,47 +33,43 @@ public class MainActivity extends AppCompatActivity {
         formatTxt = (TextView)findViewById(R.id.scan_format);
         contentTxt = (TextView)findViewById(R.id.scan_content);
 
-        scanBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        scanBtn.setOnClickListener(this);
+    }
 
-                // Получаем разрешение на камеру для Android 6+
-                int permissionStatus = ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CAMERA);
-                final int MY_CAMERA_REQUEST_CODE = 100;
-                if (permissionStatus != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {android.Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
-                }
+    @Override
+    public void onClick(View v) {
+        scanCode();
+    }
 
-                else {
-                    IntentIntegrator scanIntegrator = new IntentIntegrator(MainActivity.this);
-                    scanIntegrator.setOrientationLocked(false);
-                    // Процедура для сканирования
-                    scanIntegrator.initiateScan();
-                }
+    private void scanCode() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.initiateScan();
+
+    }
+    // Обработка результата сканирования
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        // Не пустой результат
+        if (scanningResult != null) {
+            if(scanningResult.getContents() != null) {
+                // Результат сканирования в виде строки
+                String scanContent = scanningResult.getContents();
+                // Вид barcode
+                String scanFormat = scanningResult.getFormatName();
+                // Запись в textView результата
+                formatTxt.setText(scanFormat);
+                contentTxt.setText(scanContent);
             }
-
-            // Обработка результата сканирования
-            public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-
-                IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-                // Не пустой результат
-                if (scanningResult != null) {
-                    // Результат сканирования в виде строки
-                    String scanContent = scanningResult.getContents();
-                    // Вид barcode
-                    String scanFormat = scanningResult.getFormatName();
-
-                    // Запись в textView результата
-                    formatTxt.setText("FORMAT: " + scanFormat);
-                    contentTxt.setText("CONTENT: " + scanContent);
-                }
-                else {
-                    Toast toast = Toast.makeText(getApplicationContext(),
-                            "Данные не получены!", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
+            else {
+                Toast toast = Toast.makeText(getApplicationContext(), "Данные не получены!", Toast.LENGTH_SHORT);
+                toast.show();
             }
-
-        });
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, intent);
+        }
     }
 }
