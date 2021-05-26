a613384fa8f8c1fb4b4af69886a74569d42f658b
Nick Lobashov (Laptop) <nlobashov.ru@gmail.com>
2021-05-26 15:14:51 +0500

    Получаем данные через API сайта в фоновом потоке

--- app/src/main/java/com/example/qr/Student_Control.java
+++ app/src/main/java/com/example/qr/Student_Control.java
@@ -5,16 +5,30 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.UriMatcher;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class Student_Control extends AppCompatActivity {
 
+    TextView tvName;
+    ImageView ivPhoto;
     Button Inform_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,8 @@ public class Student_Control extends AppCompatActivity {
         String studentCode = arguments.get("ean13").toString();
         ((AppCompatActivity) Student_Control.this).getSupportActionBar().setTitle("EAN-13:" + studentCode);
 
+        tvName = (TextView)findViewById(R.id.StudentName);
+        ivPhoto = (ImageView)findViewById(R.id.ProfilePic);
         Inform_button = findViewById(R.id.Inform_button);
         Inform_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +48,7 @@ public class Student_Control extends AppCompatActivity {
                 startActivity(new Intent(Student_Control.this, Student_Information.class));
             }
         });
+        new AsyncData().execute(studentCode);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,5 +61,56 @@ public class Student_Control extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    // Класс для получения данных в фоновом потоке
+    class AsyncData extends AsyncTask<String, Void, String> {
+
+        private final String API_LOGIN = "android";
+        private final String API_PASS = "KU986iu1";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                String studentCode = strings[0];
+                String apiLink = "https://team1984.nlobashov.ru/api.php";
+                String params = String.format("login=%s&pass=%s&code=%s", API_LOGIN, API_PASS, studentCode);
+
+                URL url = new URL(apiLink);
+                URLConnection connection = url.openConnection();
+                connection.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+                wr.write(params);
+                wr.flush();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                StringBuilder result = new StringBuilder();
+                String line = null;
+
+                while ((line = reader.readLine()) != null)
+                {
+                    result.append(line);
+                    break;
+                }
+                reader.close();
+                return  result.toString();
+
+            } catch (Exception e) {
+                return new String("Сервер не отвечает");
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result.equals("false"))
+            {
+                tvName.setText("Студент не найден");
+            }
+            else
+            {
+                tvName.setText(result);
+                ivPhoto.setImageDrawable(getDrawable(R.drawable.rango));
+            }
+        }
     }
 }
